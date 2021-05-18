de4930c9dad2b599d522fff3d58969c9559ebdfb
Lola <46529244+lgolin@users.noreply.github.com>
2021-05-18 12:20:29 +0100

    dashboard page

--- login-system/index.js
+++ login-system/index.js
@@ -1,21 +1,45 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const {v4: uuidv4} = require('uuid');
+const router = require('./router');
+
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
+// body-parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// ejs engine
 app.set('view engine', 'ejs');
 
-// load static assessts
-app.use('/static', express.static(path.join(__dirname, 'public'))); // style
-app.use('/assets', express.static(path.join(__dirname, 'public/assets'))); // images
+// load static assets
+app.use('/static', express.static(path.join(__dirname, 'public')));
 
-// home route
-app.get(`/`, (req, res) => {
+app.use(
+  session({
+    secret: uuidv4(),
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.locals.user = req.session.user;
+  next();
+});
+
+app.use('/route', router);
+
+// Home route
+app.get('/', (req, res) => {
   res.render('base', {title: 'Login System'});
 });
 
-// Start server
+// listen port
 app.listen(port, () =>
-  console.log('Listening to the server on http://localhost:3000')
+  console.log('Listening to the server http://localhost:5000')
 );
